2f2700e9fbe43afff41b292b2f78621bc8226dd3
Shesh Gaur <sheshg3@gmail.com>
2024-01-13 02:50:12 -0500

    Mobile Support Implemented

---
+++ sidebar-toggle.js
@@ -0,0 +1,25 @@
+
+let content = document.getElementById("toggle");
+
+function animation() {
+
+    if (content.classList.contains("fadeIn"))
+    {
+        content.classList.remove("fadeIn");
+        content.classList.add("fadeOut");
+        return;
+    }
+
+    content.classList.remove("fadeOut");
+    content.classList.add("fadeIn");  
+}
+
+
+window.onresize = function(event) {
+
+    content.classList.remove("fadeIn");
+    content.classList.remove("fadeOut");
+
+    content.classList.add("fadeIn");
+    setTimeout(500)
+};
